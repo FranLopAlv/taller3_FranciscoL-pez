c33348e6601a9d308641c6585a53a722518a6fe7
Your Name <you@example.com>
2016-09-18 18:42:50 -0400

    tarea5

---
+++ js/app.js
@@ -0,0 +1,20 @@
+function (){
+	console.log('Hola Mundo');
+}();
+var variableArray = [];
+
+var variableString = 'this is String';
+
+var variableNúmerico= 20;
+
+variableArray.push('im here');
+variableArray.push('whithout you');
+
+console.log(variableArray);
+
+var elObjeto = {};
+
+elObjeto.descripcion = 'this is Object';
+elObjeto.cantidad = 1;
+
+console.log(elObjeto);
